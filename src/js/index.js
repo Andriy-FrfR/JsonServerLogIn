31914084042d96b43c9ea9abff836a6b1f067dcf
unknown <andriy.lukashewwich@gmail.com>
2020-12-27 20:32:22 +0200

    Added auto login after registration, check for the existence of an account with the typed email before registering, saving score on database, initialization of the score after login

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,45 @@
 // Requests
 
 const logInButton = document.getElementById('log-in-button');
-let user
+let user;
+
+const signUpButton = document.getElementById('sign-up-button');
+
+signUpButton.addEventListener('click', () => {
+  fetch(`http://localhost:3000/users/?email=${document.querySelector('.email-input').value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    if (data.length !== 0) {
+      alert('Account with the email you typed already exists!')
+      return;
+    } else {
+      fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: document.querySelector('.email-input').value,
+            password: document.querySelector('.password-input').value
+          })
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        user = data;
+        score.innerHTML = 0;
+        console.log(data);
+      });
+    }
+    console.log(data);
+  })
+  .catch(() => {
+    alert('Error');
+  })
+})
 
 logInButton.addEventListener('click', () => {
   fetch(`http://localhost:3000/users/?email=${document.querySelector('.email-input').value}&password=${document.querySelector('.password-input').value}`)
@@ -15,6 +53,7 @@ logInButton.addEventListener('click', () => {
       alert('Wrong email or password');
     } else {
       user = data[0];
+      score.innerHTML = user.score || 0;
     }
     
     console.log(data);
@@ -24,34 +63,17 @@ logInButton.addEventListener('click', () => {
   })
 })
 
-const signUpButton = document.getElementById('sign-up-button');
-
-signUpButton.addEventListener('click', () => {
-    fetch('http://localhost:3000/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: document.querySelector('.email-input').value,
-          password: document.querySelector('.password-input').value
-        })
-    })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
-})
-
 // Score
 
 const scoreButton = document.getElementById('score-button'),
     score = document.getElementById('score');
 
 const scorePatch = () => {
-  fetch(`http://localhost:3000/users/${2}`, {
+  if (!user) {
+    alert('Log in or Sign up, please!');
+    return;
+  }
+  fetch(`http://localhost:3000/users/${user.id}`, {
       method: 'PATCH',
       headers: {
           'Content-Type': 'application/json'
